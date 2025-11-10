f45c3775a88d1632ed543f7f90e1cce42363e492
xanni <michealhuang113@gmail.com>
2025-11-10 08:50:21 +0700

    be5

--- BE2/Areas/Admin/Controllers/HomeController.cs
+++ BE2/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BE2.Models;
+using BE2.Models.ViewModel;
+using PagedList;
+using PagedList.Mvc;
+
 
 namespace BE2.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private MyStoreEntities db = new MyStoreEntities();
         // GET: Admin/Home
-        public ActionResult Index()
+        public ActionResult Index(string searchTerm, int? page)
         {
-            return View();
+            var model = new HomeProductVM();
+            var products = db.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                model.SearchTerm = searchTerm;
+                products = products.Where(p => p.ProductName.Contains(searchTerm) ||
+                    p.ProductDecription.Contains(searchTerm) ||
+                    p.Category.CategoryName.Contains(searchTerm));
+            }
+
+            int pageNumber = page ?? 1;
+            int pageSize = 6;
+
+            model.FeaturedProducts = products.OrderByDescending(p => p.OrderDetails.Count()).Take(10).ToList();
+
+            model.NewProducts = products.OrderBy(p => p.OrderDetails.Count()).Take(20).ToPagedList(pageNumber, pageSize);
+
+
+            return View(model);
         }
     }
 }
---
+++ BE2/Models/ViewModel/HomeProductVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList.Mvc;
+
+namespace BE2.Models.ViewModel
+{
+    public class HomeProductVM
+    {
+        public string SearchTerm { get; set; }
+
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public List<Product> FeaturedProducts { get; set; }
+
+        public PagedList.IPagedList<Product> NewProducts { get; set; }
+    }
+}
